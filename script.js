f883aaf8636984d50144cb171af4010b02471357
5goko5 <123900248+5goko5@users.noreply.github.com>
2025-02-24 21:34:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 <script src="https://cdn.jsdelivr.net/npm/@emailjs/browser@3/dist/email.min.js"></script>
-<script>
 emailjs.init("T8dFFYK-nFxpuLbbv"); // ضع هنا "Public Key" الخاص بك
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("loginForm").addEventListener("submit", function(event) {
